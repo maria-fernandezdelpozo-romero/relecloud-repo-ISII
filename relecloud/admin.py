94128cb5c4342a9e203e6fbb3cc0f549908caf9a
meripozo <9306433@alumnos.ufv.es>
2025-10-06 00:12:35 +0200

    initial commit

---
+++ relecloud/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+admin.site.register(models.Cruise)
+admin.site.register(models.Destination)
+
---
+++ relecloud/models.py
@@ -0,0 +1,40 @@
+from audioop import reverse
+from django.db import models
+
+# Create your models here.
+class Destination(models.Model):
+    name = models.CharField(
+        unique=True,
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse("destination_detail", kwargs={"pk": self.pk})
+    
+
+class Cruise(models.Model):
+    name = models.CharField(
+        unique=True,
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+    description = models.TextField(
+        max_length=2000,
+        null=False,
+        blank=False
+    )
+    destinations = models.ManyToManyField(
+        Destination,
+        related_name='cruises'
+    )
+    def __str__(self):
+        return self.name
